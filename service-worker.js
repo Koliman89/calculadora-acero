24f1123b62d7294f702647ce5f1ccfb5e90959a1
Koliman89 <victor_viveros@live.com>
2026-05-30 14:09:07 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = "calculadora-acero-v1";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
